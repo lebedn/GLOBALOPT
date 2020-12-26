7ad8bf6ad2268bd3e0eb20af95a2a624d480c59d
Natala <nata.lebed.1998@gmail.com>
2020-12-26 02:52:05 +0200

    adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -2,29 +2,14 @@ $(document).ready(function() {
     $('.reviews__carousel').slick({
         speed: 200,
         swipe: false,
-        /*     adaptiveHeight: true, */
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 
         responsive: [{
-                breakpoint: 991,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            },
-            {
                 breakpoint: 575,
                 settings: {
-                    dots: true,
-                    arrows: false
+                    dots: false,
+                    arrows: true
                 }
             }
 
@@ -32,3 +17,20 @@ $(document).ready(function() {
 
     });
 });
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
